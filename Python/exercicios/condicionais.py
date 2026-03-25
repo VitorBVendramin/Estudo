08782690ea8af6c4ed2cdea1a388038d85744b24
VitorBVendramin <vitorvbrunelli@outlook.com>
2026-03-25 15:08:26 -0300

    primeiro exercicio de condicional

---
+++ Python/exercicios/condicionais.py
@@ -0,0 +1,8 @@
+# Verificando se pode comprar o passaporte ou não
+
+Dinheiro_passaporte = int(input("Coloque quanto tem de dinheiro atual: ")) # Usuário coloca a quantia de dinheiro que tem para a compra
+
+if Dinheiro_passaporte >= 2500:
+    print("Você comprou o passaporte!")
+else:
+    print("Você não possuí a quantia necessária para a compra!")
