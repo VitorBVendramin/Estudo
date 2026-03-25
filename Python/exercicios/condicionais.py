e4eb5b134c800c8c803ba372d43bc3bf331f8da1
VitorBVendramin <vitorvbrunelli@outlook.com>
2026-03-25 15:37:31 -0300

    comentando cada função

--- Python/exercicios/condicionais.py
+++ Python/exercicios/condicionais.py
@@ -10,11 +10,11 @@
 
 
 
-valor_carteira = int(input("Insira o saldo da carteira digital: ")) # Usuário insere o valor da carteira digital
+valor_carteira = int(input("Insira o saldo da carteira digital: ")) # Usuário insere o valor da carteira digital.
 
 if valor_carteira >= 100:
-    print("Você concluiu a compra com sucesso!")
+    print("Você concluiu a compra com sucesso!") # Caso usuário tenha 100 reais ou mais, compra será realizada.
 elif valor_carteira == 0:
-    print("Você não tem nenhum saldo restante!")
+    print("Você não tem nenhum saldo restante!") # Caso usuário não tenha nenhum saldo.
 else:
-    print(f"Você não tem dinheiro suficiente para esta compra, porém ainda tem {valor_carteira} de saldo sobrando para uso!")
+    print(f"Você não tem dinheiro suficiente para esta compra, porém ainda tem {valor_carteira} de saldo sobrando para uso!") # Caso usuário tenha entre 99 a 1 real de saldo.
