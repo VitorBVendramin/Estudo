faaf1695425e55ea812b491caa507b4827aab6da
VitorBVendramin <vitorvbrunelli@outlook.com>
2026-04-09 22:59:14 -0300

    trocando nome da pasta projeto e comentando no exerc de rep01

--- Python/exercicios/repeticao_ex1.py
+++ Python/exercicios/repeticao_ex1.py
@@ -1,11 +1,11 @@
-senha_correta = "12345"
+senha_correta = "12345" # Valor da senha correta
 senha = ""
 
-for senha in range(5):
-    senha = input("digite a senha: ")
-    if senha == senha_correta:
+for senha in range(5): # Limite de 5 tentativas
+    senha = input("digite a senha: ") # Onde o usuário coloca a senha
+    if senha == senha_correta: # Só libera o acesso se a senha for exatamento igual ao valor
         print("Acesso liberado")
         break
 
 else:
-    print("Limite excedido, tente novamente mais tarde!")
+    print("Limite excedido, tente novamente mais tarde!") # Caso a pessoa tente mais de 5x o código atinge o limite
