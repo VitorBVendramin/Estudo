5f1cc65b90379014d7ca9e95975d2d42abac5cbf
VitorBVendramin <vitorvbrunelli@outlook.com>
2026-04-10 17:49:29 -0300

    adicionando dificuldade facil e dificil

--- Python/projetos/jogo-adivinhacao.py
+++ Python/projetos/jogo-adivinhacao.py
@@ -1,17 +1,32 @@
 # Jogo da advinhação
 
+numero_secreto = 30
+tentativas = 0
+chute = 0
+dificuldade = 0
 
-# Regra do jogo: Acertar o número secreto, com dicas ou sem dicas, escolha sua própria dificuldade
+dificuldade = int(input("Escolha a dificuldade: 1 = Fácil/ 2 = Difícil: "))
 
-Numero_secreto = 30
-tentativa = 0
+if dificuldade == 1:
+    tentivas_max = 15
+    # dicas normais
 
-while tentativa != Numero_secreto:
-    tentativa = int(input("Adivinhe o número de 1 a 100: "))
+elif dificuldade == 2:
+    tentivas_max = 10
+    # dicas enigmaticas
+    
 
-    if  tentativa < Numero_secreto:
+
+while chute != numero_secreto and tentativas < tentivas_max:
+    chute = int(input("Adivinhe o número de 1 a 100: "))
+    tentativas += 1
+
+    if  chute < numero_secreto:
         print("O número secreto é maior!")
-    elif tentativa > Numero_secreto:
+    elif chute > numero_secreto:
         print("O número secreto é menor!")
     else:
         print("Você acertou, Parabéns!")
+
+if chute != numero_secreto:
+    print("Você perdeu! O número secreto era", numero_secreto)
