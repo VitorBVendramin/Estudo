d6615836a16c1bb70dd80eaf540b6b7baa8eaa26
VitorBVendramin <vitorvbrunelli@outlook.com>
2026-04-16 17:00:58 -0300

    finalizando projeto caixa-eletronico e subindo README

--- Python/projetos/caixa-eletronico/caixa-eletronico.py
+++ Python/projetos/caixa-eletronico/caixa-eletronico.py
@@ -1,25 +1,26 @@
-saldo = 1000
-extrato = []
+from datetime import datetime
 
-def sacar(saldo):
+def sacar(saldo, extrato):
     sacar = float(input("Digite o valor do saque: "))
+    horario = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 
     if sacar > saldo:
         print("Saldo insuficiente, operação não realizada")
     else:
         saldo -= sacar
-        print(f"Saque efetuado! Novo saldo é de:R${saldo:.2f}")
+        extrato.append(f"Saque de R${sacar:.2f} - {horario}")
 
     return saldo
 
-def depositar(saldo):
+def depositar(saldo, extrato):
     depositar = float(input("Digite o valor do depósito: "))
+    horario = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 
     if depositar <= 0:
         print("Nenhum depósito foi feito, pois o número é menor que zero!")
     else:
         saldo += depositar
-        print(f"Depósito efetuado com succeso, seu novo saldo é de:R${saldo:.2f}")
+        extrato.append(f"Depósito de:R${depositar:.2f} - {horario}")
     
     return saldo
 
@@ -28,10 +29,16 @@ def consultar_saldo(saldo):
     print(f"Seu saldo atual é de:R${saldo:.2f}")
 
 def ver_extrato(extrato):
-    
+    if not extrato:
+        print("Nenhuma operação foi realizada ainda!")
+    else:
+        for operacao in extrato:
+            print(operacao)
 
 
 def menu():
+    saldo = 1000
+    extrato = []
     while True:
         print("=== CAIXA ELETRÔNICO ===")
         print("1 - Sacar")
@@ -43,21 +50,23 @@ def menu():
         opcao = int(input("Escolha uma opção: "))
 
         if opcao == 1:
-            saldo = sacar(saldo)
+            saldo = sacar(saldo, extrato)
 
         elif opcao == 2:
-            saldo = depositar(saldo)
+            saldo = depositar(saldo, extrato)
     
         elif opcao == 3:
-            consultar_saldo()
+            consultar_saldo(saldo)
     
         elif opcao == 4:
-            ver_extrato()
+            ver_extrato(extrato)
         
         elif opcao == 5:
             break
 
         else:
             print("Número digitado é inválido, escolha entre 1 a 5!")
+
+menu()
 
 print("Operação finalizada!")
