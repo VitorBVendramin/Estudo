f8fb1df3e7dea1ebfe84e57a6436b7dc17d5643b
VitorBVendramin <vitorvbrunelli@outlook.com>
2026-09-21 22:00:23 -0300

    testando variaveis

---
+++ Python/projetos/producao-garrafas/main.py
@@ -0,0 +1,13 @@
+valor_anterior = 45000
+
+valor_atual = int(input("Digite o valor atual de garrafas da máquina: "))
+
+valor_total = valor_anterior + valor_atual
+
+diferença_valor = valor_atual - valor_anterior
+
+diferença_valor/400
+
+print(f"A quantia de garrafa por hora é de: {diferença_valor}")
+
+print(f"A quantia de garrafa atual é de: {valor_total}")
